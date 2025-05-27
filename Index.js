a6819bbe61be62aa180b22e3419dc3076c40b4e8
Stalkedx <artandrei221@gmail.com>
2025-05-28 07:40:21 +0800

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { checkItemPrice } from './checkItemPrice.js'; // adjust path if needed
+import { checkItemPrice } from './priceLogic.js'; // adjust path if needed
 
 dotenv.config();
 
